978cc5e31cad9767e1ff976fbcf53f0835e7defd
Pozdrawiam <witam@tuta.io>
2023-07-04 21:27:46 +0200

    File list - two direction sorting

--- Wfm.Domain/Features/FileManager/GetFiles/GetFilesHandler.cs
+++ Wfm.Domain/Features/FileManager/GetFiles/GetFilesHandler.cs
@@ -27,9 +27,34 @@ public class GetFilesHandler
 
         IEnumerable<FileSystemEntry> entries = _fileSystemService.GetEntries(path)
             .Where(x => x.RelativePath != ThumbnailConsts.DirName)
-            .Select(x => x with { RelativePath = Path.Join(query.RelativePath, x.RelativePath) })
-            .OrderBy(x => x.Name);
+            .Select(x => x with { RelativePath = Path.Join(query.RelativePath, x.RelativePath) });
 
+        if (!string.IsNullOrWhiteSpace(query.OrderBy))
+            entries = OrderEntries(entries, query.OrderBy, query.OrderDesc);
+        
         return new GetFilesResult(query.LocationIndex, query.RelativePath, entries);
+    }
+
+    private static IEnumerable<FileSystemEntry> OrderEntries(IEnumerable<FileSystemEntry> entries, string orderBy, bool orderDesc)
+    {
+        var orderByMapping = new Dictionary<string, Func<FileSystemEntry, object>>()
+        {
+            { nameof(FileSystemEntry.Name), x => x.Name },
+            { nameof(FileSystemEntry.Size), x => x.Size },
+            { nameof(FileSystemEntry.ModifiedDate), x => x.ModifiedDate },
+            { nameof(FileSystemEntry.Extension), x => x.Extension }
+        };
+
+        if (orderByMapping.ContainsKey(orderBy))
+        {
+            Func<FileSystemEntry, object> orderByExpression = orderByMapping[orderBy];
+
+            if (orderDesc)
+                entries = entries.OrderByDescending(orderByExpression);
+            else
+                entries = entries.OrderBy(orderByExpression);
+        }
+
+        return entries;
     }
 }
--- Wfm.Domain/Features/FileManager/GetFiles/GetFilesQuery.cs
+++ Wfm.Domain/Features/FileManager/GetFiles/GetFilesQuery.cs
@@ -1,3 +1,3 @@
 namespace Wfm.Domain.Features.FileManager.GetFiles;
 
-public record GetFilesQuery(int LocationIndex, string RelativePath);
+public record GetFilesQuery(int LocationIndex, string RelativePath, string? OrderBy, bool OrderDesc);
